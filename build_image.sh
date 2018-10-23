45933fa1c98b9bfcac1da44a2ba71f0f7fe14af4
Hrishikesh N <hrishikeshn@ncbs.res.in>
2018-10-23 17:58:29 +0530

    Added Script to build and run images.

--- build_image.sh
+++ build_image.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 docker image build \
 	--build-arg http_proxy=http://proxy.ncbs.res.in:3128 \
+	--build-arg https_proxy=http://proxy.ncbs.res.in:3128 \
 	-t bhallalab/deepcut_centos7 \
 	.
