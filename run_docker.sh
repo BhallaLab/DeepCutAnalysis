bfaea023db8b940ec3a1b7a04d2ed87b86cd558b
Dilawar Singh <dilawars@ncbs.res.in>
2018-10-23 18:25:45 +0530

    Fixed.

--- run_docker.sh
+++ run_docker.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -x
 set -e
-GPU=1 bash ./dlc-docker run \
-	-d -p 2351:8888 \
-	-e USER_HOME=$HOME/Work/DeepLabCut \
-	--name GPU0 \
-	bhallalab/deepcut_centos7
-
+GPU=1 nvidia-docker run \
+	-v $HOME/Work:/root/Work \
+	-it bhallalab/deepcut_centos7 \
+        /bin/bash
