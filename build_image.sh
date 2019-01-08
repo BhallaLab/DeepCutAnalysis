3ceba425f42c0ddf1ed0ab15fc40e807a5609e63
Hrishikesh N <hrishikeshn@ncbs.res.in>
2019-01-08 14:47:20 +0530

    Version 2.

--- build_image.sh
+++ build_image.sh
@@ -2,5 +2,5 @@
 docker image build \
 	--build-arg http_proxy=http://proxy.ncbs.res.in:3128 \
 	--build-arg https_proxy=http://proxy.ncbs.res.in:3128 \
-	-t bhallalab/deepcut_centos7 \
+	-t bhallalab/deeplabcut2 \
 	.
