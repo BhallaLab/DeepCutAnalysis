2a2e38670ad340db19bc4f90f7fb3ef1c7799fba
Dilawar Singh <dilawars@ncbs.res.in>
2019-01-09 12:13:48 +0530

    Small tweaks.

--- run_docker.sh
+++ run_docker.sh
@@ -3,7 +3,6 @@ set -x
 set -e
 GPU=1 nvidia-docker run \
 	-v $HOME/Work:/root/Work \
-	-it bhallalab/deeplabcut2:latest \
 	-e DISPLAY=$DISPLAY \
         -v /tmp/.X11-unix:/tmp/.X11-unix \
-        /bin/bash
+	-it bhallalab/deeplabcut2:latest /bin/bash
