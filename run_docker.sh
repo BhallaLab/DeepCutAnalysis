f2c88ea2138d943a81b2513a597c25421d0acc4b
Dilawar Singh <dilawars@ncbs.res.in>
2019-01-09 12:26:16 +0530

    Fixes X11 forwarding issue.

--- run_docker.sh
+++ run_docker.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -x
 set -e
+# How to run X11: https://dzone.com/articles/docker-x11-client-via-ssh
 GPU=1 nvidia-docker run \
 	-v $HOME/Work:/root/Work \
-	-e DISPLAY=$DISPLAY \
+        --net=host \
+	--env="DISPLAY" \
+        --volume="$HOME/.Xauthority:/root/.Xauthority:rw" \
         -v /tmp/.X11-unix:/tmp/.X11-unix \
 	-it bhallalab/deeplabcut2:latest /bin/bash
