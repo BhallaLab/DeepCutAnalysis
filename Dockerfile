20bb719dd4700b05db660265523c2164e9187d2c
Dilawar Singh <dilawars@ncbs.res.in>
2019-03-09 10:32:46 +0530

    Install wxpython in docker.

--- Dockerfile
+++ Dockerfile
@@ -18,3 +18,4 @@ RUN pip install ipywidgets
 RUN pip3 install ipywidgets
 
 RUN pip3 install seaborn
+RUN ./install_wxpython.sh
---
+++ install_wxpython.sh
@@ -0,0 +1,17 @@
+#!/bin/bash 
+
+apt install \
+    dpkg-dev \
+    build-essential \
+    libjpeg-dev \
+    libtiff-dev \
+    libsdl1.2-dev \
+    libgstreamer-plugins-base0.10-dev \
+    libnotify-dev \
+    freeglut3 \
+    freeglut3-dev \
+    libsm-dev \
+    libgtk-3-dev \
+    libwebkitgtk-3.0-dev \
+    libxtst-dev
+python3 -m pip install wxpython
